996804dc82465c6af139ac34429bc68564ce9de3
claffer2 <claffer2@naz.edu>
2016-09-12 16:32:39 -0400

    Small tweaks.

--- src/setup.py
+++ src/setup.py
@@ -60,17 +60,11 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='docker development',
+    keywords='yaml development',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['docs', 'tests']),
-
-    package_data={
-        '': [
-            'docker/*'
-        ]
-    },
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
